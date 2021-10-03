2333bc8afee0881b7e0d22ef0d7f8a9e1aead3f5
Barzabel <kochenko1995@mail.ru>
2021-10-04 00:09:56 +0300

    add argparse

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+
+build:
+	poetry build;
+
+publish:
+	poetry publish --dry-run
+
+
+package-install:
+	python3.9 -m pip install hexlet_code-0.1.0-py3-none-any.whl
+
+package-upgrade:
+	python3.9 -m pip install --upgrade --user dist/hexlet_code-0.1.0-py3-none-any.whl;
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+args = parser.parse_args()
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+	main()
