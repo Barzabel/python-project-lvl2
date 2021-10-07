04a2629e93230683c31862057ddec62a131a547c
Barzabel <kochenko1995@mail.ru>
2021-10-07 08:28:07 +0300

    add gendiff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Barzabel/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Barzabel/python-project-lvl2/actions)
+
+
+ 
+презентация работы gendiff
+[![asciicast](https://asciinema.org/a/440531.png )](https://asciinema.org/a/440531)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,4 @@
+from ..gen_diff import generate_diff
 import argparse
 
 
@@ -5,15 +6,11 @@ parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
 
-parser.add_argument('-f', '--format', action='store_true', 
-    help="-f FORMAT, --format FORMAT set format of output")
-
-
+parser.add_argument('-f', '--format', help="-f FORMAT, --format FORMAT set format of output")
 args = parser.parse_args()
 
 def main():
-    pass
+    print(generate_diff(args.first_file, args.second_file))
 
-
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__' :
+    main()
