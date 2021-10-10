412a1b567d05d628c32f628570846d98980ef7da
Barzabel <kochenko1995@mail.ru>
2021-10-10 10:08:40 +0300

    add test diff json

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -25,7 +25,7 @@ def generate_diff(path1, path2):
                 res += "\n  - {}: {}".format(x,data1[x])
                 res += "\n  + {}: {}".format(x,data2[x])
         elif x in data1 and x not in data2:
-            res += "\n  - {}:{}".format(x,data1[x])
+            res += "\n  - {}: {}".format(x,data1[x])
         elif x not in data1 and x in data2:
             res += "\n  + {}: {}".format(x,data2[x])
     res += "\n}"
@@ -33,38 +33,3 @@ def generate_diff(path1, path2):
 
 
 
-"""
-
-import json
-from pathlib import Path
-
-
-def get_path(path):
-    cwd_dir = Path.cwd()
-    res_path = Path(cwd_dir, path)
-    return res_path
-
-
-def generate_diff(path1, path2):
-    with open(get_path(path1), "r") as read_file:
-        data1 = json.load(read_file)
-    with open(get_path(path2), "r") as read_file:
-        data2 = json.load(read_file)
-    keys = set(data1.keys()) | set(data2.keys())
-    keys = list(keys)
-    keys.sort()
-    res = "{"
-    for x in keys:
-        if x in data1 and x in data2:
-            if data1[x] == data2[x]:
-                res += f"\n    {x}: {data1[x]}"
-            else:
-                res += f"\n  - {x}: {data1[x]}"
-                res += f"\n  + {x}: {data2[x]}"
-        elif x in data1 and x not in data2:
-            res += f"\n  - {x}:{data1[x]}"
-        elif x not in data1 and x in data2:
-            res += f"\n  + {x}: {data2[x]}"
-    res += "\n}"
-    return res
-"""
--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -2,5 +2,6 @@ import pytest
 from gendiff.gen_diff import generate_diff
 
 def test_one():
-	generate_diff('tests/fixtures/1.json', 'tests/fixtures/2.json')
-	assert True
+    res = '{\n    age: 35\n  + cat: True\n    children: True\n    firstName: Jane\n    hobbies: False\n  - lalala: 1\n  - lastName: Doe\n  + lastName: andr\n}'
+    assert generate_diff('tests/fixtures/1.json', 'tests/fixtures/2.json') == res
+
