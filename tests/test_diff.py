25f6418ed6a414cb8f2da879040c89999fa3d252
Barzabel <kochenko1995@mail.ru>
2021-10-10 20:46:30 +0300

    revue test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,11 +1,16 @@
 import pytest
 from gendiff.gen_diff import generate_diff
 
-def test_json():
-    res = '{\n    age: 35\n  + cat: True\n    children: True\n    firstName: Jane\n    hobbies: False\n  - lalala: 1\n  - lastName: Doe\n  + lastName: andr\n}'
-    assert generate_diff('tests/fixtures/1.json', 'tests/fixtures/2.json') == res
 
-def test_yaml():
-    res = '{\n    age: 35\n  + cat: True\n    children: True\n    firstName: Jane\n    hobbies: False\n  - lalala: 1\n  - lastName: Doe\n  + lastName: andr\n}'
-    assert generate_diff('tests/fixtures/1.yaml', 'tests/fixtures/2.yml') == res
+@pytest.fixture
+def result():
+    return '{\n    age: 35\n  + cat: True\n    children: True\n    firstName: Jane\n    hobbies: False\n  - lalala: 1\n  - lastName: Doe\n  + lastName: andr\n}'
+
+
+
+def test_json(result):
+    assert generate_diff('tests/fixtures/1.json', 'tests/fixtures/2.json') == result
+
+def test_yaml(result):
+    assert generate_diff('tests/fixtures/1.yaml', 'tests/fixtures/2.yml') == result
 
