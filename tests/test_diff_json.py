d9a4fcb5cd5bec96161467fd03c46db9c62a0cda
Barzabel <kochenko1995@mail.ru>
2021-10-20 22:01:35 +0300

    fexed

--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -9,10 +9,17 @@ def result_for_deap():
         return json.load(read_file)
 
 
-def test_json():
-    assert generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap1.json', "json") == ''
+@pytest.fixture
+def empaty_json_format():
+    with open('tests/fixtures/empaty_json_format.json', "r") as read_file:
+        return json.load(read_file)
 
 
-def test_json_plain(result_for_deap):
+def test_json(empaty_json_format):
+    res = generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap1.json', "json") 
+    assert res == '{0}\n'.format(json.dumps(empaty_json_format, indent=4))
+
+
+def test_json_deap(result_for_deap):
     res = generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap2.json', "json")
     assert res == '{0}\n'.format(json.dumps(result_for_deap, indent=4))
