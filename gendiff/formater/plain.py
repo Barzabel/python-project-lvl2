a6b38fd7dd9cdb9ab632ea4dc3c8ab3f453881a4
Barzabel <kochenko1995@mail.ru>
2021-10-20 21:42:56 +0300

    fexed

--- gendiff/formater/plain.py
+++ gendiff/formater/plain.py
@@ -40,20 +40,24 @@ def _recurs_for_key(data, parent):
 
 def plain(data):
     new_data = _recurs_for_key(data, '')
-    res = ""
+    result = []
     for x in range(len(new_data)):
         value = serialize_value_plain(new_data[x]['value'])
         status = new_data[x]["status"]
         key = new_data[x]['key']
         is_end = x < len(new_data) - 1
+        line = None
         if new_data[x]["status"] == 1:
-            line = "\nProperty '{}' was added with value: {}"
-            res += line.format(key, value)
+            line = "Property '{}' was added with value: {}"
+            line = line.format(key, value)
         elif status == -1 and is_end and key == new_data[x + 1]['key']:
             v_next = serialize_value_plain(new_data[x + 1]['value'])
-            line = "\nProperty '{}' was updated. From {} to {}"
-            res += line.format(key, value, v_next)
+            line = "Property '{}' was updated. From {} to {}"
+            line = line.format(key, value, v_next)
             new_data[x + 1]['status'] = None
         elif new_data[x]["status"] == -1:
-            res += "\nProperty '{}' was removed".format(new_data[x]['key'])
-    return res
+            line = "Property '{}' was removed".format(new_data[x]['key'])
+
+        if line:
+            result.append(line)
+    return '\n'.join(result)
--- tests/test_diff_plain.py
+++ tests/test_diff_plain.py
@@ -4,6 +4,7 @@ from gendiff.gendiff import generate_diff
 
 @pytest.fixture
 def result_for_deap():
+    print(generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap1.json', "plain"))
     with open('tests/fixtures/right_answer_plain', "r") as read_file:
         return read_file.read()
 
