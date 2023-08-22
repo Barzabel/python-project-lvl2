d8093ecf43e5a6c90b959abff3f411168782018f
Barzabel <kochenko1995@mail.ru>
2023-08-22 22:06:00 +0300

    Refactoring a recursive function in diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,101 +4,66 @@ from .formater.stylish import stylish
 from .formater.json_format import json_formatter
 
 
-def _rec_diff2(data1, data2):
+def _get_dict(status, key, value):
+    return {
+        "status": status,
+        "key": key,
+        "value": value
+    }
+"""
+1 добавить статусы key
+2 длинна строчек
+3 сделать явный базовый случий для рекурсивной функции *
+
+"""
+
+def _rdiff(data1, data2):
+    if type(data2) != dict or type(data1) != dict: 
+        return data1
+
     keys = set(data1.keys()) | set(data2.keys())
     keys = list(keys)
     keys.sort()
     res = []
-    for x in keys:
-        if x in data1 and x in data2:
-            if data1[x] == data2[x]:
-                if type(data1[x]) == dict:
-                    res.append({
-                        "status": 0,
-                        "key": x,
-                        "value": _rec_diff2(data1[x], data2[x])
-                    })
-                else:
-                    res.append({
-                        "status": 0,
-                        "key": x,
-                        "value": data1[x]
-                    })
-            else:
-                if type(data1[x]) == dict and type(data2[x]) != dict:
-                    res.append({
-                        "status": -1,
-                        "key": x,
-                        "value": _rec_diff2(data1[x], data1[x])
-                    })
-                    res.append({
-                        "status": +1,
-                        "key": x,
-                        "value": data2[x]
-                    })
+    status = 0
 
-                elif type(data1[x]) != dict and type(data2[x]) == dict:
-                    res.append({
-                        "status": -1,
-                        "key": x,
-                        "value": data1[x]
-                    })
-                    res.append({
-                        "status": +1,
-                        "key": x,
-                        "value": _rec_diff2(data2[x], data2[x])
-                    })
-                elif type(data1[x]) == dict and type(data2[x]) == dict:
-                    res.append({
-                        "status": 0,
-                        "key": x,
-                        "value": _rec_diff2(data1[x], data2[x])
-                    })
+    for key in keys:
+        value_diff = None
+        if key not in data1:
+            status = 1
+            value_diff = data2[key]
+        elif key not in data2:
+            status = -1
+            value_diff = data1[key]
+        else:
+            status = 0
+            value_diff = data1[key], data2[key]
+
+        if status == 0:
+            if data1[key] == data2[key]:
+                if type(data1[key]) == dict:
+                    res.append(_get_dict(status, key, _rdiff(*value_diff)))
                 else:
-                    res.append({
-                        "status": -1,
-                        "key": x,
-                        "value": data1[x]
-                    })
-                    res.append({
-                        "status": 1,
-                        "key": x,
-                        "value": data2[x]
-                    })
-        elif x in data1 and x not in data2:
-            if type(data1[x]) == dict:
-                res.append({
-                    "status": -1,
-                    "key": x,
-                    "value": _rec_diff2(data1[x], data1[x])
-                })
+                    res.append(_get_dict(status, key, data1[key]))
             else:
-                res.append({
-                    "status": -1,
-                    "key": x,
-                    "value": data1[x]
-                })
-        elif x not in data1 and x in data2:
-            if type(data2[x]) == dict:
-                res.append({
-                    "status": +1,
-                    "key": x,
-                    "value": _rec_diff2(data2[x], data2[x])
-                })
+                if type(data1[key]) == dict and type(data2[key]) == dict:
+                    res.append(_get_dict(status, key, _rdiff(*value_diff)))
+                else:
+                    res.append(_get_dict(-1, key, _rdiff(data1[key], data1[key])))
+                    res.append(_get_dict(1, key, _rdiff(data2[key], data2[key])))
+        else:
+            if type(value_diff) == dict:
+                res.append(_get_dict(status, key, _rdiff(value_diff, value_diff)))
             else:
-                res.append({
-                    "status": 1,
-                    "key": x,
-                    "value": data2[x]
-                })
+                res.append(_get_dict(status, key, value_diff))
     return res
 
 
 def generate_diff(path1, path2, formator="stylish"):
     data1, data2 = get_dict_from_path(path1, path2)
     if formator == 'stylish':
-        return stylish(_rec_diff2(data1, data2))
+        return stylish(_rdiff(data1, data2))
     elif formator == 'plain':
-        return plain(_rec_diff2(data1, data2))
+        return plain(_rdiff(data1, data2))
     elif formator == 'json':
-        return json_formatter(_rec_diff2(data1, data2))
+        return json_formatter(_rdiff(data1, data2))
