af43f1855bd2c37ea30845af872d116abacc5ed5
Barzabel <Kochenko1995@mail.ru>
2023-09-01 07:34:16 +0300

    change file parser

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -1,12 +1,5 @@
 import json
 import yaml
-from pathlib import Path
-
-
-def get_path(path):
-    cwd_dir = Path.cwd()
-    res_path = Path(cwd_dir, path)
-    return res_path
 
 
 def parser_data(data, format_file='json') -> dict:
@@ -19,5 +12,5 @@ def parser_data(data, format_file='json') -> dict:
 
 def get_dict_from_path(path1):
     format_file = path1.split('.')[-1]
-    with open(get_path(path1), "r") as data:
+    with open(path1, "r") as data:
         return parser_data(data, format_file)
