50da9a9c394b36ac5583b9518470cff92ef59320
Barzabel <Kochenko1995@mail.ru>
2023-09-08 06:59:29 +0300

    change tests, add pytest.mark.parametrize

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,48 +1,67 @@
 import pytest
 import json
+import os
 from gendiff.gendiff import generate_diff
 
 
-@pytest.fixture
-def result_json_empty():
-    with open('tests/fixtures/empty_json_format.json', "r") as read_file:
-        return '{0}\n'.format(json.dumps(json.load(read_file), indent=4))
+def get_data_from_file(path):
+    root, ext = os.path.splitext(path)
+    with open(path, "r") as read_file:
+        if ext == ".json":
+            return '{0}\n'.format(json.dumps(json.load(read_file), indent=4))
+        else:
+            return read_file.read()
 
 
-@pytest.fixture
-def result_within_json():
-    with open('tests/fixtures/right_answer_json.json', "r") as read_file:
-        return '{0}\n'.format(json.dumps(json.load(read_file), indent=4))
+@pytest.mark.parametrize(
+    argnames='prepared_files',
+    argvalues=[
+        [
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/empty_json_format.json',
+            'json'
+        ],
+        [
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/empty_plain_format',
+            'plain'
+        ],
+        [
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/file_deap2.json',
+            'tests/fixtures/right_answer_json.json',
+            'json'
+        ],
+        [
+            'tests/fixtures/file_deap1.json',
+            'tests/fixtures/file_deap2.json',
+            'tests/fixtures/right_answer_plain',
+            'plain'
+        ],
+        [
+            'tests/fixtures/file_deap1.yml',
+            'tests/fixtures/file_deap2.yml',
+            'tests/fixtures/right_answer_stylish',
+            'stylish'
+        ],
+    ],
+)
+def test_stylish(prepared_files):
+    file1_path, file2_path, result_render_path, answer_type = prepared_files
+    result_render = get_data_from_file(result_render_path)
+    assert result_render == generate_diff(
+        file1_path,
+        file2_path,
+        answer_type
+    )
 
 
-@pytest.fixture
-def result_within_plain():
-    with open('tests/fixtures/right_answer_plain', "r") as read_file:
-        return read_file.read()
-
-
-@pytest.fixture
-def result_within_stylish():
-    with open('tests/fixtures/right_answer_stylish', "r") as read_file:
-        return read_file.read()
-
-
-@pytest.fixture
-def result_stylish():
-    return '{\n    age: 35\n  + cat: true\n    children: true\n    firstName: Jane\n    hobbies: false\n  - lalala: 1\n  - lastName: Doe\n  + lastName: andr\n}'
-
-
-def test_empty_result(result_json_empty):
-    empty_json = generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap1.json', "json")
-    assert empty_json == result_json_empty
-    assert generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap1.json', "plain") == ''
-
-
-def test_json_within_json(result_within_json, result_within_plain, result_within_stylish):
-    assert generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap2.json', "json") == result_within_json
-    assert generate_diff('tests/fixtures/file_deap1.json', 'tests/fixtures/file_deap2.json', "plain") == result_within_plain
-    assert generate_diff('tests/fixtures/file_deap1.yml', 'tests/fixtures/file_deap2.yml') == result_within_stylish
-
-
-def test_yaml(result_stylish):
-    assert generate_diff('tests/fixtures/1.yaml', 'tests/fixtures/2.yml') == result_stylish
+@pytest.mark.parametrize(
+    argnames='test_input,expected',
+    argvalues=[(('tests/fixtures/1.yaml', 'tests/fixtures/2.yml'),
+                'tests/fixtures/right_answer_yml'), ],
+)
+def test_yaml(test_input, expected):
+    assert generate_diff(*test_input) == get_data_from_file(expected)
