ac3e3427314032d6bcc74b26cf5c436e467f1103
Barzabel <Kochenko1995@mail.ru>
2023-09-12 07:20:32 +0300

    fixed parms in fun parser

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -3,10 +3,10 @@ import yaml
 import os
 
 
-def parse(data, format_file='.json') -> dict:
-    if format_file == '.json':
+def parse(data, format_file='json') -> dict:
+    if format_file == 'json':
         dict_from_data = json.load(data)
-    elif format_file == '.yml' or format_file == '.yaml':
+    elif format_file == 'yml' or format_file == 'yaml':
         dict_from_data = yaml.safe_load(data)
     else:
         raise Exception("invalid file format")
@@ -16,4 +16,4 @@ def parse(data, format_file='.json') -> dict:
 def get_dict_from_path(path):
     root, ext = os.path.splitext(path)
     with open(path, "r") as data:
-        return parse(data, ext)
+        return parse(data, ext[1:])
