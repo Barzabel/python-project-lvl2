70583d9a024269d7c5f363334da20b931160d49c
Barzabel <Kochenko1995@mail.ru>
2023-09-24 18:29:54 +0300

    change stulish

--- gendiff/formater/stylish.py
+++ gendiff/formater/stylish.py
@@ -5,7 +5,7 @@ CHANGED = "changed"
 UNCHANGED = "unchanged"
 
 
-def join_value_plain(value):
+def join_value_stylish(value):
     if isinstance(value, bool):
         value = str(value).lower()
     elif value is None:
@@ -13,21 +13,26 @@ def join_value_plain(value):
     return value
 
 
+def get_line_str(status, key, value, deap):
+    line = ""
+    if UNCHANGED in status:
+        line = "{}    {}: {}".format(deap, key, value)
+    elif ADDED in status:
+        line = "{}  + {}: {}".format(deap, key, value)
+    elif DELETED in status:
+        line = "{}  - {}: {}".format(deap, key, value)
+    return line
+
+
 def stylish(data, deap=""):
     res = ["{"]
     for x in data:
-        line = ""
         if NESTED in x["status"]:
             new_deap = "    " + deap
             value = stylish(x["value"], new_deap)
         else:
-            value = join_value_plain(x["value"])
-        if UNCHANGED in x["status"]:
-            line = "{}    {}: {}".format(deap, x['key'], value)
-        elif ADDED in x["status"]:
-            line = "{}  + {}: {}".format(deap, x['key'], value)
-        elif DELETED in x["status"]:
-            line = "{}  - {}: {}".format(deap, x['key'], value)
+            value = join_value_stylish(x["value"])
+        line = get_line_str(x["status"], x['key'], value, deap)
         res.append(line)
     end = deap + "}"
     res.append(end)
