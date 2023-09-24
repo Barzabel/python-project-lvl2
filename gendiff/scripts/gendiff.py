248aa0077922a57b15e2568db3697204728441d6
Barzabel <Kochenko1995@mail.ru>
2023-09-24 11:04:33 +0300

    add new class ArgparseForGendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,23 @@
 from gendiff.gendiff import generate_diff
+import argparse
 from gendiff.arg_pars_for_gendiff import ArgparseForGendiff
 
 
 args = ArgparseForGendiff().get_args()
 
 
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+
+parser.add_argument('-f', '--format', default="stylish", help="-f FORMAT, \
+    --format FORMAT set format of output")
+args = parser.parse_args()
+
+
 def main():
-    print(generate_diff(
-        args['first_file'],
-        args['second_file'],
-        args['formator']))
-    print(generate_diff(
-        args['first_file'],
-        args['second_file'],
-        args['formator']))
+    print(generate_diff(args.first_file, args.second_file, args.format))
+
 
 if __name__ == '__main__':
     main()
