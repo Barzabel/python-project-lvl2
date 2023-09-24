608636bbf09060edaad7ee9a81d0fdd8558591ef
Barzabel <Kochenko1995@mail.ru>
2023-09-24 10:38:14 +0300

    add new class ArgparseForGendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ args = ArgparseForGendiff()
 
 def main():
     print(args['formator'])
-    print(generate_diff(args['first_file'], args['second_file'], args['formator'])
+    print(generate_diff(args['first_file'], args['second_file'], args['formator']))
 
 
 if __name__ == '__main__':
