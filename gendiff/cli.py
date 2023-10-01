b96ced25c3e5ed023040e5854279a4dbd0a9b72b
Barzabel <Kochenko1995@mail.ru>
2023-10-01 08:39:24 +0300

    fixed cli

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,18 +1,15 @@
 import argparse
 
 
-class ArgparseForGendiff:
-    def __init__(self):
-        self.parser = argparse.ArgumentParser(description='Generate diff')
-        self.parser.add_argument('first_file', type=str)
-        self.parser.add_argument('second_file', type=str)
-        self.parser.add_argument('-f', '--format', default="stylish", help="-f FORMAT, \
+def get_args():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', default="stylish", help="-f FORMAT, \
     --format FORMAT set format of output")
-
-    def get_args(self):
-        args = self.parser.parse_args()
-        return {
-            'first_file': args.first_file,
-            'second_file': args.second_file,
-            'formator': args.format
-        }
+    args = parser.parse_args()
+    return {
+        'first_file': args.first_file,
+        'second_file': args.second_file,
+        'formator': args.format
+    }
