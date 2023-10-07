d9ac7e1931e94e6ca8d2127447f49afcfddf95ab
Barzabel <Kochenko1995@mail.ru>
2023-10-07 08:30:46 +0300

    fixed plain

--- gendiff/formater/plain.py
+++ gendiff/formater/plain.py
@@ -24,26 +24,27 @@ def get_path(path, key):
         return key
 
 
-def plain(data, path=""):
+def plain(data):
     result = []
-    for var in data:
-        new_path = get_path(path, var["key"])
-        status = var["status"]
-        value = serialize_value(var['value'])
-        res_line = None
-        if NESTED == status:
-            result.extend(plain(var["value"], new_path))
-        elif CHANGED == status:
-            v_ex = serialize_value(var['old_value'])
-            res_line = "Property '{}' was updated. From {} to {}"
-            res_line = res_line.format(new_path, v_ex, value)
-            result.append(res_line)
-        elif ADDED == status:
-            res_line = "Property '{}' was added with value: {}"
-            res_line = res_line.format(new_path, value)
-            result.append(res_line)
-        elif DELETED == status:
-            res_line = "Property '{}' was removed".format(new_path)
-            result.append(res_line)
-
-    return "\n".join(result) if path == "" else result
+    def wolk(data, path=""):
+        for var in data:
+            new_path = get_path(path, var["key"])
+            status = var["status"]
+            value = serialize_value(var['value'])
+            res_line = None
+            if NESTED == status:
+                wolk(var["value"], new_path)
+            elif CHANGED == status:
+                v_ex = serialize_value(var['old_value'])
+                res_line = "Property '{}' was updated. From {} to {}"
+                res_line = res_line.format(new_path, v_ex, value)
+                result.append(res_line)
+            elif ADDED == status:
+                res_line = "Property '{}' was added with value: {}"
+                res_line = res_line.format(new_path, value)
+                result.append(res_line)
+            elif DELETED == status:
+                res_line = "Property '{}' was removed".format(new_path)
+                result.append(res_line)
+    wolk(data, path="")
+    return "\n".join(result)
