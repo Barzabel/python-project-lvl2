5c11fa67b5019ea4f4252a1c93c71fdaafde1be2
Barzabel <Kochenko1995@mail.ru>
2023-10-20 07:42:23 +0300

    changed parser

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -3,16 +3,15 @@ import yaml
 import os
 
 
-def parse(data, format_file='json') -> dict:
-    if format_file == 'json':
-        return json.load(data)
-    elif format_file == 'yml' or format_file == 'yaml':
-        return yaml.safe_load(data)
-
-    raise Exception("invalid file format")
+PARSERS = {
+    '.yaml': lambda x: yaml.safe_load(x),
+    '.yml': lambda x: yaml.safe_load(x),
+    '.json': lambda x: json.load(x),
+}
 
 
 def get_dict_from_path(path):
     root, ext = os.path.splitext(path)
+    parser = PARSERS[ext]
     with open(path, "r") as data:
-        return parse(data, ext[1:])
+        return parser(data)
